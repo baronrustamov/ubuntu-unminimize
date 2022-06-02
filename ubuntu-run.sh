aef7e643308bba73596a8683d844adc95c86560c
Vladislav <sorokinvld@gmail.com>
2022-06-02 20:59:37 +0300

    cmt

--- ubuntu-run.sh
+++ ubuntu-run.sh
@@ -3,6 +3,12 @@
 # https://askubuntu.com/questions/1705/how-can-i-create-a-select-menu-in-a-shell-script
 #docker run -p 8080:80 --rm -it -v -v /shapiro/Documents/vscode/:/home/user/ ubuntu-run
 
+#-e TZ=UTC	Timezone.
+#-p 8080:80	Expose Nginx on localhost:8080.
+#-v /local/path/to/website:/var/www/html	Mount and serve a local website.
+#-v /path/to/conf.template:/etc/nginx/templates/conf.template	Mount template files inside /etc/nginx/templates. They will be processed and the results will be placed at /etc/nginx/conf.d. (e.g. listen ${NGINX_PORT}; will generate listen 80;).
+#-v /path/to/nginx.conf:/etc/nginx/nginx.conf
+
 docker image rm ubuntu-run
 docker build -t ubuntu-run . 
 docker run -p 8080:80 --rm -it -v /Users/shapiroisme/Documents/vscode/:/home/user/ ubuntu-run
